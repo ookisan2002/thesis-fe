ef12a1a6640dc5f7d84403a435e37b0e5fb89802
Ooki san <104672099+ookisan2002@users.noreply.github.com>
2025-01-07 12:44:12 +0700

    Update page.tsx

--- src/components/CustomeCard/page.tsx
+++ src/components/CustomeCard/page.tsx
@@ -26,7 +26,7 @@ export function CustomeCarCard({
     <div
       key={key}
       {...props}
-      className={`${key} p-[1.88rem] tablet:p-[2.88rem] h-fit bg-white shadow-[6px_0px_39.768px_0px_rgba(0,0,0,0.05)] rounded-[0.9375rem] tablet:rounded-[1.9375rem] ${className}`}
+      className={`${key} h-auto flex flex-col p-[1.88rem] tablet:p-[2.88rem] bg-white shadow-[6px_0px_39.768px_0px_rgba(0,0,0,0.05)] rounded-[0.9375rem] tablet:rounded-[1.9375rem] ${className}`}
     >
       <h5 className='text-[1.25rem] tablet:text-[3.25rem] font-semibold leading-[150%] mb-[2.06rem]'>
         {title}
@@ -43,13 +43,13 @@ export function CustomeCarCard({
           {desRow2}
         </p>
       </div>
-      <div className='flex items-center'>
+      <div className='flex items-center mb-[1.31rem]'>
         <Mail className='[&_*]:stroke-primary size-[1.375rem] tablet:size-[2.375rem] mr-[0.75rem]' />
         <p className='text-[0.9375rem] tablet:text-[1.9375rem] leading-[160%] font-normal text-[#444]'>
           {desRow3}
         </p>
       </div>
-      <div className='w-full flex justify-between pt-[1.37rem] mt-[1.31rem] border-t-[0.04375rem] border-t-[#44444480]'>
+      <div className='w-full flex justify-between pt-[1.37rem] border-t-[0.04375rem] border-t-[#44444480] mt-auto'>
         <p className='text-[#828282] text-[0.9375rem] tablet:text-[1.9375rem] leading-[160%] font-semibold'>
           Ngày đăng kí: <br />
           {subDes}
